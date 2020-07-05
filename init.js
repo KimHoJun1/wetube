2935428166b1f6c02a0ee2adf9d00512c863e8ab
hjkim <hhaa407@naver.com>
2020-07-06 00:37:42 +0900

    db complete

--- init.js
+++ init.js
@@ -1,8 +1,13 @@
 import app from "./app";
+import "./db";
+import dotenv from "dotenv";
+dotenv.config();
+import "./models/Video";
+import "./models/Comments";
 
-const PORT = 4000;
+const PORT = process.env.PORT || 4000;
 
 const handleListening = () =>
-  console.log(`Listening on: http://localhost:${PORT}`);
+  console.log(`✔ Listening on: http://localhost:${PORT}`);
 
 app.listen(PORT, handleListening);
